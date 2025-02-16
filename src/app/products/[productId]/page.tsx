072ab6440b69f18a3c90d3b821dcb258e01213dc
Nil Rajpurohit <nilrajp1318@gmail.com>
2025-02-16 23:39:49 +0530

    Routing Metadata and Dynamic Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string;
+    }
+}
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`Product ${params.productId}`);
+        }, 1000);
+    });
+    
+    return {
+        title: ``+title,
+    }
+}
+
 export default function ProductDetails({ params }: {
     params: {productId: string}
 }) {
