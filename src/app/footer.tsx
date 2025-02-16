629969b1fcde3402ae72de3bd14e86ef0b263bca
Nil Rajpurohit <nilrajp1318@gmail.com>
2025-02-16 22:55:12 +0530

    Layout, Nested Layout, Group Layout.

---
+++ src/app/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <>
+        <h1 style={{ background: "black", color: "white"}}>Header</h1>
+        </>
+    )
+}
---
+++ src/app/header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <>
+        <h1 style={{ background: "black", color: "white"}}>Header</h1>
+        </>
+    )
+}
